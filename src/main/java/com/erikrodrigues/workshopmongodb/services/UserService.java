4466fb8c06b927de03705e2376a8b609e67199c4
ErikRodrigues <erickviips16@gmail.com>
2021-07-06 15:01:12 -0300

    Criando metodo POST

--- src/main/java/com/erikrodrigues/workshopmongodb/services/UserService.java
+++ src/main/java/com/erikrodrigues/workshopmongodb/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.erikrodrigues.workshopmongodb.domain.User;
+import com.erikrodrigues.workshopmongodb.dto.UserDTO;
 import com.erikrodrigues.workshopmongodb.repository.UserRepository;
 import com.erikrodrigues.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional <User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto (UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
